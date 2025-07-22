d5fef033af039c44e39970d07a9dbb85d54d04d2
Kicho <elchiqvo@gmail.com>
2025-07-22 14:12:06 -0500

    added propper indentation and cleaed imports

--- frontend/components/PageHeader/PageHeader.jsx
+++ frontend/components/PageHeader/PageHeader.jsx
@@ -1,56 +1,46 @@
-'use client';
+"use client";
 
-import styles from './PageHeader.module.css';
-import { H1 } from '@leafygreen-ui/typography';
-import Button, { Variant, Size } from '@leafygreen-ui/button';
-import Icon from '@leafygreen-ui/icon';
-import {
-  MongoDBLogo,
-  MongoDBLogoMark,
-  
-} from '@leafygreen-ui/logo';
-import InfoWizard from '../InfoWizard/InfoWizard';
-import { useState } from 'react';
-import talktrackDemo from '@/talkTrack/talkbackBuilder.js';
-import { usePathname } from 'next/navigation'
+import styles from "./PageHeader.module.css";
+import { H1 } from "@leafygreen-ui/typography";
+import { MongoDBLogoMark } from "@leafygreen-ui/logo";
+import InfoWizard from "../InfoWizard/InfoWizard";
+import { useState } from "react";
+import talktrackDemo from "@/talkTrack/talkbackBuilder.js";
+import { usePathname } from "next/navigation";
 
 const PageHeader = () => {
-
   const [openHelpModal, setOpenHelpModal] = useState(false);
 
-
   const pathname = usePathname();
-  
 
   return (
-    
-
     <header className={styles.wrapper}>
-        <div className={styles.leftLogo}> 
-        <MongoDBLogoMark height={70} width={70} color='green-base' />
-        </div>
-        
+      <div className={styles.leftLogo}>
+        <MongoDBLogoMark height={70} width={70} color="green-base" />
+      </div>
+
       <div className={styles.center}>
-        <img src="/General_INDUSTRY_Auto10x.png" alt="" width={70} height={70}/>
+        <img
+          src="/General_INDUSTRY_Auto10x.png"
+          alt=""
+          width={70}
+          height={70}
+        />
 
         <H1>Leafy Fleet</H1>
       </div>
 
       <div className={styles.right}>
         <InfoWizard
-        open={openHelpModal}
-        setOpen={setOpenHelpModal}
-        tooltipText="Tell me more!"
-        iconGlyph="Wizard" 
-        sections={talktrackDemo(pathname)}
+          open={openHelpModal}
+          setOpen={setOpenHelpModal}
+          tooltipText="Tell me more!"
+          iconGlyph="Wizard"
+          sections={talktrackDemo(pathname)}
         />
-
       </div>
     </header>
   );
 };
 
 export default PageHeader;
-
-
-
--- frontend/talkTrack/talkbackBuilder.js
+++ frontend/talkTrack/talkbackBuilder.js
@@ -1,11 +1,7 @@
 import BTSTalktrackSection from "./talktracks/BTSTalktrack.js";
 import chartsTalktrackSection from "./talktracks/chartsTalktrack.js";
 import chatTalktrackSection from "./talktracks/chatTalktrack.js";
-import bodyComponent, {
-  htmlBodyComponent,
-} from "./components/bodyComponent.js";
 import {
-  createTalktrackSection,
   combineTalktrackSections,
 } from "./components/talktrackComponent.js";
 import overviewTalktrackSection from "./talktracks/overviewTalktrack.js";
@@ -35,7 +31,6 @@ export function talktrackDemo(talktrackRequirement) {
     );
   }
   return combineTalktrackSections(
-    chatTalktrackSection(),
     BTSTalktrackSection(),
     whyMongoDBTalktrackSection()
   );
