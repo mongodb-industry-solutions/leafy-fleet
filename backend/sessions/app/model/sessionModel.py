fd3d8c6c24395a3cb51427287c0a37ac4f3577b3
fermofou <fermofou@gmail.com>
2025-08-13 20:43:00 -0500

    mejorar mensajes open api

--- backend/sessions/app/model/sessionModel.py
+++ backend/sessions/app/model/sessionModel.py
@@ -1,31 +1,13 @@
-from random import random
 import string
-from pydantic import ConfigDict, BaseModel, Field
+from pydantic import BaseModel, Field
 from typing import  List
 from typing_extensions import Annotated
-from bson import ObjectId
-from pymongo import ReturnDocument
+
 from pydantic.functional_validators import BeforeValidator
 from datetime import datetime
-from pydantic import BaseModel, Field  
-from typing import Optional  
-PyObjectId = Annotated[str, BeforeValidator(str)]
+from typing import Optional 
 
-# Custom ObjectId type for Pydantic  
-class PyObjectId(ObjectId):  
-    @classmethod  
-    def __get_validators__(cls):  
-        yield cls.validate  
-  
-    @classmethod  
-    def validate(cls, v):  
-        if not ObjectId.is_valid(v):  
-            raise ValueError("Invalid ObjectId")  
-        return ObjectId(v)  
-  
-    @classmethod  
-    def __modify_schema__(cls, field_schema):  
-        field_schema.update(type="string") 
+PyObjectId = Annotated[str, BeforeValidator(str)]
 
 # Nested Class for the Telemetry/Timeseries Model
 class Fleet(BaseModel):  
@@ -35,11 +17,15 @@ class Fleet(BaseModel):
     fleet_size: List[int] = Field(default_factory=list)  # List of fleet IDs [50,70,90], vechiles 0 - 50, 100 - 170, 200-290
     attribute_list: List[List[str]] = Field(default_factory=list)  # List of attributes for each fleet [["attribute1", "attribute2"], ["attribute3", "attribute4"]]
 class Message(BaseModel):
-    message: str = Field(default="easter egg") #text
+    message: str = Field(default="any question") #text
     index: int = Field(default=0) #from 0 to n, to kniow the order of the messages. in theory should not be needed if appended to list in order, but just in case
     sender: str = Field(default="bot") #bot or user
     completed: bool = Field(default=True) #if the message is completed or not, to know if the bot is still typing
-    
+class MessageEntry(BaseModel):
+    thread_id: PyObjectId = Field(default_factory=PyObjectId)  # Unique identifier for the message thread
+    message: str = Field(..., description="The content of the message")
+    sender: str = Field(..., description="The sender of the message (e.g., 'user' or 'bot')")
+    completed: bool = Field(default=True, description="Indicates if the message is completed or not")
 
 class SessionModel(BaseModel):
     """
